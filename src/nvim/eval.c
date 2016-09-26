5950c5e793e6d326a2ca1b053f75b894e1c668b4
Tae Won Ha <h@taewon.de>
2016-09-26 18:34:21 +0200

    Remove gui_running

--- src/nvim/eval.c
+++ src/nvim/eval.c
@@ -10519,7 +10519,6 @@ static void f_has(typval_T *argvars, typval_T *rettv, FunPtr fptr)
     "gettext",
 #ifdef CUSTOM_UI
     "gui_vimr",
-    "gui_running",
 #endif
 #if defined(HAVE_ICONV_H) && defined(USE_ICONV)
     "iconv",
